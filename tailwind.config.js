d70b111a31032ac073e97ed715ed4e9ba8fde1b1
Leandro Cotti <lefcott@hotmail.com>
2022-05-07 19:53:11 -0300

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/projects/**/pages/**/*.{ts,tsx}",
+    "./src/projects/**/components/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
