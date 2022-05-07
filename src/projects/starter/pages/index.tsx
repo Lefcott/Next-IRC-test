1b094be43e9258a84bf8abc1639f23a5487540e1
Leandro Cotti <lefcott@hotmail.com>
2022-05-07 20:41:29 -0300

    absolute imports

--- src/projects/starter/pages/index.tsx
+++ src/projects/starter/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Test from "../../../components/Test";
+import Test from "src/components/Test";
 
 const Home: NextPage = () => {
   return (
