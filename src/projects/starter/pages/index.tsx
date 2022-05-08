f55e355642c0880d69061e68a438183c87dfe0d4
Leandro Cotti <lefcott@hotmail.com>
2022-05-08 18:56:26 -0300

    added ts plugin

--- src/projects/starter/pages/index.tsx
+++ src/projects/starter/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Test from 'src/components/Test';
 import Test2 from '@/components/Test';
 
-notdefinedvariableee;
+notdefinedvariableeeee;
 
 const Home: NextPage = () => {
   return (
