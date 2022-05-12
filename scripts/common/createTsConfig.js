eb9ca7c6824d34b1aa35bd2ec20e847080314b36
Leandro Cotti <lefcott@hotmail.com>
2022-05-11 22:24:56 -0300

    used tsconfig at the root dir

--- scripts/common/createTsConfig.js
+++ scripts/common/createTsConfig.js
@@ -1,17 +1,18 @@
-const fs = require("fs");
-const path = require("path");
-const tsconfig = require("../../tsconfig.base.json");
+const fs = require('fs');
+const path = require('path');
+const tsconfig = require('../../tsconfig.json');
 
 const createTsConfig = (projectPath) => {
   if (!tsconfig.compilerOptions.paths) {
     tsconfig.compilerOptions.paths = {};
   }
 
-  tsconfig.compilerOptions.paths["@/*"] = [`${projectPath}/*`];
+  tsconfig.compilerOptions.baseUrl = '../../..';
+  tsconfig.compilerOptions.paths['@/*'] = [`${projectPath}/*`];
 
   fs.writeFileSync(
-    path.join(projectPath, "tsconfig.json"),
-    `${JSON.stringify(tsconfig, null, 2)}\n`
+    path.join(projectPath, 'tsconfig.json'),
+    `${JSON.stringify(tsconfig, null, 2)}\n`,
   );
 };
 
